ac312571b7631218171019b106f55ace24a4602f
axelstram <axelstraminsky@gmail.com>
2024-03-29 16:01:57 -0300

    added DatabaseManager class to comunicate with the Postgresql db

---
+++ db/src/db_manager.py
@@ -0,0 +1,126 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+from ..models.user import User
+from ..models.expenses import Expense
+
+
+class DatabaseManager:
+    def __init__(self, database=None, host=None, port=None, user=None, password=None):
+        load_dotenv()
+
+        self.conn = psycopg2.connect(
+            host = host or os.getenv('DB_HOST'),
+            port = port or os.getenv('DB_PORT'),
+            user = user or os.getenv('DB_USER'),
+            password = password or os.getenv('DB_PASSWORD'),
+            database = database or os.getenv('DB_DATABASE')
+        )
+        self.conn.autocommit = True
+        self.cursor = self.conn.cursor()
+
+    def __exit__(self):
+        self.close_connection()
+
+    def create_tables(self):
+
+        users_table_sql = '''
+            CREATE TABLE IF NOT EXISTS users (
+            "id" SERIAL PRIMARY KEY,
+            "telegram_id" TEXT UNIQUE NOT NULL
+            );''';
+
+        expenses_table_sql = '''
+            CREATE TABLE IF NOT EXISTS expenses (
+                "id" SERIAL PRIMARY KEY,
+                "user_id" INTEGER NOT NULL REFERENCES users("id"),
+                "description" TEXT NOT NULL,
+                "amount" MONEY NOT NULL,
+                "category" TEXT NOT NULL,
+                "added_at" TIMESTAMP NOT NULL
+            );''';
+
+        self.execute_query(users_table_sql)
+        self.execute_query(expenses_table_sql)
+
+        create_user_id_index_sql = '''CREATE INDEX IF NOT EXISTS user_id_idx
+                                        ON expenses(user_id);'''
+
+        self.execute_query(create_user_id_index_sql)
+
+        self.conn.commit()
+
+    def delete_tables(self):
+        delete_expenses_sql = '''DROP TABLE IF EXISTS expenses;'''
+        delete_usrs_sql = '''DROP TABLE IF EXISTS users;'''
+
+        self.cursor.execute(delete_expenses_sql)
+        self.cursor.execute(delete_usrs_sql)
+
+    def close_connection(self):
+        self.cursor.close()
+        self.conn.close()
+        
+    def execute_query(self, query):
+        try:
+            self.cursor.execute(query)
+
+            if 'SELECT' in query:
+                return self.cursor.fetchall()
+            else:
+                #nothing to fetch
+                return True
+        except Exception as e:
+            print(f"Error executing query: {e}")
+            return False
+
+
+    def insert_user(self, user):
+        try:
+            query = "INSERT INTO users (telegram_id) VALUES ({0})".format(user.get_telegram_id())
+            self.cursor.execute(query)
+            return True
+        except Exception as e:
+            print(f"Error adding user: {e}")
+            return False
+
+
+    def insert_expenses(self, expenses):
+        try:
+            query = """SELECT id FROM users
+                        WHERE telegram_id = '{0}';""".format(expenses.user_telegram_id)
+
+            result = self.execute_query(query)
+
+            id = result[0][0]
+
+            query = """INSERT INTO expenses (user_id, description, amount, category, added_at) 
+                        VALUES (%i, '%s', %i, '%s', '%s')""" % (id, expenses.description, expenses.amount, expenses.category, expenses.added_at)
+
+            self.execute_query(query)
+            
+            return True
+        except Exception as e:
+            print(f"Error adding expenses: {e}")
+            return False
+
+    def get_expenses_from_user(self, user):
+        try:
+            query = """SELECT id FROM users
+                        WHERE telegram_id = '{0}';""".format(user.telegram_id)
+
+            result = self.execute_query(query)
+
+            id = result[0][0]
+
+            query = """SELECT * FROM expenses
+                        WHERE user_id = '{0}';""".format(id)
+
+            result = self.execute_query(query)
+            
+            return result
+            
+        except Exception as e:
+            print(f"Error getting expenses from user: {e}")
+            return False
+
