2209ca8be1178944e1a4d6c4fda5e7bdf109ec57
axelstram <axelstraminsky@gmail.com>
2024-04-01 18:24:27 -0300

    updated Bot interface description

--- bots/interfaces/bot.py
+++ bots/interfaces/bot.py
@@ -3,7 +3,8 @@ from abc import ABC, abstractmethod
 class Bot(ABC):
 
     #sends the message to the bot for classification
-    #returns dict with 'Category', 'Amount' and 'Description' as keys
+    #returns dict with 'Category', 'Amount' and 'Description' as keys if message was related to expenses
+    #if not, it return a dict with 'Category' = None
     @abstractmethod
     def process_message(self, message) -> dict:
         pass
