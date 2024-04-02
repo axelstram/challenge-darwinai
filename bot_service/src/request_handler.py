862f683c2954aa07f184a940ec6062fc58585651
axelstram <axelstraminsky@gmail.com>
2024-04-01 22:19:37 -0300

    .

--- bot_service/src/request_handler.py
+++ bot_service/src/request_handler.py
@@ -60,7 +60,3 @@ async def list_user_expenses(user_id):
     else:
         return {f'user {user_id} has no expenses'}
 
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
