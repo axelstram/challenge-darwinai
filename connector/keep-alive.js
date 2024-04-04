7942204fbce024d576daff72f846d679fdd22ceb
axelstram <axelstraminsky@gmail.com>
2024-04-04 14:17:57 -0300

    try again back4app

--- connector/keep-alive.js
+++ connector/keep-alive.js
@@ -1,11 +1,12 @@
-const http = require('http');
+const https = require('https');
 
 const TARGET_URL = 'https://connector-ubxv3t5y.b4a.run/';
-const INTERVAL = 25 * 60 * 1000; // 25 minutes in milliseconds
+const PORT = process.env.PORT || 80;
+const INTERVAL = 5 * 60 * 1000; // 5 minutes in milliseconds
 
 function keepAlive() {
-  http.get(TARGET_URL, () => {
-    // Do nothing 
+  https.get(`${TARGET_URL}:${PORT}`  , () => {
+    console.log('keep alive');
   });
 }
 
--- connector/src/connector.js
+++ connector/src/connector.js
@@ -9,7 +9,7 @@ if (process.env.PRODUCTION != true) {
 const token = process.env.TELEGRAM_BOT_API_KEY;
 const BOT_TYPE = process.env.BOT_TYPE;
 const bot = new TelegramBot(token, {polling: true});
-const URL = 'https://darwinai-n3s5o067.b4a.run';
+const BOT_SERVICE_URL = process.env.BOT_SERVICE_URL
 const IS_WHITELISTED_ENDPOINT = '/is_whitelisted';
 const LIST_USER_EXPENSES_ENDPOINT = '/list_user_expenses';
 const WHITELIST_USER_ENDPOINT = '/whitelist_user';
@@ -19,7 +19,7 @@ const PROCESS_MESSAGE_ENDPOINT = '/process_message';
 
 
 async function isWhitelisted(userId) {
-    const response =  await axios.get(URL + IS_WHITELISTED_ENDPOINT + '?user_id=' + userId);
+    const response =  await axios.get(BOT_SERVICE_URL + IS_WHITELISTED_ENDPOINT + '?user_id=' + userId);
 
     if (response.data[0].includes('True')) {
         return true;
@@ -52,7 +52,7 @@ bot.on('message', async (msg) => {
 
         //whitelist user
         case '/whitelist':
-            axios.post(URL + WHITELIST_USER_ENDPOINT + '?user_id=' + userId).then(response => {
+            axios.post(BOT_SERVICE_URL + WHITELIST_USER_ENDPOINT + '?user_id=' + userId).then(response => {
                 bot.sendMessage(chatId, response.data[0]);
             })
 
@@ -63,7 +63,7 @@ bot.on('message', async (msg) => {
             whitelisted = await isWhitelisted(userId);
 
             if (whitelisted) {
-                axios.get(URL + LIST_USER_EXPENSES_ENDPOINT + '?user_id=' + userId).then(response => {
+                axios.get(BOT_SERVICE_URL + LIST_USER_EXPENSES_ENDPOINT + '?user_id=' + userId).then(response => {
                     bot.sendMessage(chatId, printExpenses(response.data['expenses']));
                 })
             } else {
@@ -79,7 +79,7 @@ bot.on('message', async (msg) => {
         
         //List all available bots
         case '/bots':
-            axios.get(URL + LIST_AVAILABLE_BOTS_ENDPOINT).then(response => {
+            axios.get(BOT_SERVICE_URL + LIST_AVAILABLE_BOTS_ENDPOINT).then(response => {
                 bot.sendMessage(chatId, 'The bots available are the following: ' + response.data['bots']);
             })
             break;
@@ -95,9 +95,9 @@ bot.on('message', async (msg) => {
 
             if (whitelisted) {
                 //Set a bot instance
-                await axios.post(URL + SET_BOT_ENDPOINT + '?bot_type=' + BOT_TYPE);
+                await axios.post(BOT_SERVICE_URL + SET_BOT_ENDPOINT + '?bot_type=' + BOT_TYPE);
 
-                axios.post(URL + PROCESS_MESSAGE_ENDPOINT + '?user_id=' + userId + '&message=' + '"' + msg.text + '"').then(response => {
+                axios.post(BOT_SERVICE_URL + PROCESS_MESSAGE_ENDPOINT + '?user_id=' + userId + '&message=' + '"' + msg.text + '"').then(response => {
                     bot.sendMessage(chatId, response.data[0]);
                 })
             } else {
